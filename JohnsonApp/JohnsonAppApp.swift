d09c7c90f234661272793ae22086b80f7e9874a2
diana <d.tsisaruk@ukma.edu.ua>
2026-06-30 23:13:47 +0300

    Initial Commit

---
+++ JohnsonApp/JohnsonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JohnsonAppApp.swift
+//  JohnsonApp
+//
+//  Created by Діана Цісарук on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct JohnsonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
